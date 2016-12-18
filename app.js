37fd31af13d371f668f8e613770ba59a3a07cb60
Paula Sun <shsun@seas.upenn.edu>
2016-12-18 16:17:20 +0000

    update company pick up

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-<<<<<<< HEAD
-var palantir = require('./routes/palantir');
-var week = require('./routes/week');
-=======
+
 var zero_three = require('./routes/zero_three');
 var three_six = require('./routes/three_six');
 var six_nine = require('./routes/six_nine');
@@ -18,7 +15,7 @@ var twelve_fifteen = require('./routes/twelve_fifteen');
 var fifteen = require('./routes/fifteen');
 var eighteen = require('./routes/eighteen');
 var twentyone_zero = require('./routes/twentyone_zero');
->>>>>>> 9afd4d36442a9ef6f08046bf8b76a18af70b7c03
+var week = require('./routes/week');
 
 var app = express();
 
@@ -35,10 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-<<<<<<< HEAD
-app.use('/palantir', palantir);
-app.use('/week', week);
-=======
+
 app.use('/zero_three', zero_three);
 app.use('/three_six', three_six);
 app.use('/six_nine', six_nine);
@@ -47,7 +41,7 @@ app.use('/twelve_fifteen', twelve_fifteen);
 app.use('/fifteen', fifteen);
 app.use('/eighteen', eighteen);
 app.use('/twentyone_zero', twentyone_zero);
->>>>>>> 9afd4d36442a9ef6f08046bf8b76a18af70b7c03
+app.use('/week', week);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
